b790e7ee8de780c5660f39e46c6208d302f13ff7
Robert Raaijmakers <rraaijmakers@bol.com>
2026-02-09 18:19:36 +0100

    Staging

--- scripts/finalize-package.js
+++ scripts/finalize-package.js
@@ -2,6 +2,7 @@ const fs = require("fs");
 const path = require("path");
 
 const rootDir = path.resolve(__dirname, "..");
+const distDir = path.join(rootDir, "dist");
 const packageLockPath = path.join(rootDir, "package-lock.json");
 const shrinkwrapPath = path.join(rootDir, "npm-shrinkwrap.json");
 
@@ -15,5 +16,11 @@ if (!fs.existsSync(packageLockPath)) {
 const packageLock = JSON.parse(fs.readFileSync(packageLockPath, "utf8"));
 fs.writeFileSync(shrinkwrapPath, JSON.stringify(packageLock, null, 2));
 
+if (!fs.existsSync(distDir)) {
+  fs.mkdirSync(distDir, { recursive: true });
+}
+fs.copyFileSync(shrinkwrapPath, path.join(distDir, "npm-shrinkwrap.json"));
+
 console.log("Generated npm-shrinkwrap.json for dependency locking.");
+console.log("Copied npm-shrinkwrap.json to dist.");
 console.log("\nPackage finalize completed.");
