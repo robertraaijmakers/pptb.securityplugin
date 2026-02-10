18e9abea5c633b92a878806f3ce20dc1f06a06fa
Robert Raaijmakers <rraaijmakers@bol.com>
2026-02-10 14:24:54 +0100

    Fix shrinkwrap location

--- scripts/finalize-package.js
+++ scripts/finalize-package.js
@@ -2,7 +2,6 @@ const fs = require("fs");
 const path = require("path");
 
 const rootDir = path.resolve(__dirname, "..");
-const distDir = path.join(rootDir, "dist");
 const packageLockPath = path.join(rootDir, "package-lock.json");
 const shrinkwrapPath = path.join(rootDir, "npm-shrinkwrap.json");
 
@@ -16,11 +15,5 @@ if (!fs.existsSync(packageLockPath)) {
 const packageLock = JSON.parse(fs.readFileSync(packageLockPath, "utf8"));
 fs.writeFileSync(shrinkwrapPath, JSON.stringify(packageLock, null, 2));
 
-if (!fs.existsSync(distDir)) {
-  fs.mkdirSync(distDir, { recursive: true });
-}
-fs.copyFileSync(shrinkwrapPath, path.join(distDir, "npm-shrinkwrap.json"));
-
 console.log("Generated npm-shrinkwrap.json for dependency locking.");
-console.log("Copied npm-shrinkwrap.json to dist.");
 console.log("\nPackage finalize completed.");
