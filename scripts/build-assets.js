40e3ba42fffee6be1052ff0658251ce680248347
Robert Raaijmakers <rraaijmakers@bol.com>
2026-02-10 13:15:18 +0100

    Updated build

---
+++ scripts/build-assets.js
@@ -0,0 +1,18 @@
+const fs = require("fs");
+const path = require("path");
+
+const rootDir = path.resolve(__dirname, "..");
+const distDir = path.join(rootDir, "dist");
+const filesToCopy = ["src/index.html", "src/index.css", "README.md"];
+
+if (!fs.existsSync(distDir)) {
+  fs.mkdirSync(distDir, { recursive: true });
+}
+
+for (const relativePath of filesToCopy) {
+  const sourcePath = path.join(rootDir, relativePath);
+  const destPath = path.join(distDir, path.basename(relativePath));
+  fs.copyFileSync(sourcePath, destPath);
+}
+
+console.log("Copied static assets to dist.");
